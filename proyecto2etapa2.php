05a369861b72ea6e4f73db6f0a7b6d78bd439d32
Alberto <random_141630@hotmail.com>
2020-11-08 23:20:48 +0100

    He añadido comentarios a los ficheros

--- proyecto2etapa2.php
+++ proyecto2etapa2.php
@@ -16,10 +16,13 @@
                 <?php /*Abro el archivo de texto con el fopen, con el while repito, mientras queden tareas, el mostrar una tarea con el fgets y el echo y hacer un salto de línea con el br , lo cierro con el fclose */
                 $fichero = fopen("pendientes.txt","rb");
                 $contador = 0;
+                /*Recorro todas las líneas del fichero con el while, en la variable $tareas creo una lista dividiendo cada línea por ; el primer elemento que sería [0] es para el id
+                y el segundo el elemento que sería [1] es para la tarea*/
                 while($a = fgets($fichero)){
                 $tareas = explode(";", $a);
                 $id = $tareas[0];
-                
+                /*En este if estoy comprobando que el id que leo en el documento sea igual que la variable $contador, cuando ambos son 0 muestro en el html el primer elemtno que sería el 0,
+                y así consecutivamente par amostrarlos todos ordenados*/
                     if($id == $contador){
                         echo $id;
                         echo $tareas[1];
@@ -30,6 +33,7 @@
                 }
                 fclose($fichero); 
                 ?>
+                <!-- Creo un form para mandar por action(post) al php donde están las verificaciones de que las tareas están correctamente rellenadas.-->
                 <form method="post"
                 enctype="application/x-wwww-form-urlencoded"
                 action="proyecto2tareas.php">
@@ -40,20 +44,46 @@
             <td>
                 <?php /*Abro el archivo de texto con el fopen, con el while repito, mientras queden tareas, el mostrar una tarea con el fgets y el echo y hacer un salto de línea con el br , lo cierro con el fclose */
                 $fichero = fopen("enprogreso.txt","rb");
-                while($tarea = fgets($fichero)){
-                echo $tarea;
-                echo "<br>";
-                }
+                $contador = 0;
+                /*Recorro todas las líneas del fichero con el while, en la variable $tareas creo una lista dividiendo cada línea por ; el primer elemento que sería [0] es para el id
+                y el segundo el elemento que sería [1] es para la tarea*/
+                while($a2 = fgets($fichero2)){
+                    $tareas = explode(";", $a2);
+                    $id = $tareas[0];
+                    /*En este if estoy comprobando que el id que leo en el documento sea igual que la variable $contador, cuando ambos son 0 muestro en el html el primer elemtno que sería el 0,
+                    y así consecutivamente par amostrarlos todos ordenados*/
+                        if($id == $contador){
+                            echo $id;
+                            echo $tareas[1];
+                            echo "<br>";
+                            $contador++;   
+                            fseek($fichero2, 0);  
+                        }
+    
+                    }
                 fclose($fichero); 
                 ?>
             </td>
             <td>
                 <?php /*Abro el archivo de texto con el fopen, con el while repito, mientras queden tareas, el mostrar una tarea con el fgets y el echo y hacer un salto de línea con el br , lo cierro con el fclose */
                 $fichero = fopen("finalizadas.txt","rb");
-                while($tarea = fgets($fichero)){
-                echo $tarea;
-                echo "<br>";
-                }
+                $contador = 0;
+                /*Recorro todas las líneas del fichero con el while, en la variable $tareas creo una lista dividiendo cada línea por ; el primer elemento que sería [0] es para el id
+                y el segundo el elemento que sería [1] es para la tarea*/
+                while($a3 = fgets($fichero3)){
+                    $tareas = explode(";", $a3);
+                    $id = $tareas[0];
+                    /*En este if estoy comprobando que el id que leo en el documento sea igual que la variable $contador, cuando ambos son 0 muestro en el html el primer elemtno que sería el 0,
+                    y así consecutivamente par amostrarlos todos ordenados*/
+                        if($id == $contador){
+                            echo $id;
+                            echo $tareas[1];
+                            echo "<br>";
+                            $contador++;   
+                            fseek($fichero3, 0);  
+                        }
+    
+                    }
                 fclose($fichero); 
                ?>
             </td>
--- proyecto2tareas.php
+++ proyecto2tareas.php
@@ -1,12 +1,16 @@
 <?php
+// Abro el fichero y con un explode la variable $tareas creo una lista dividiendo cada línea por ; el primer elemento que sería [0] es para el id
+// y el segundo el elemento que sería [1] es para la tarea
 $tareas = $_POST['nuevatarea'];
 $a = fopen("pendientes.txt","a+b");
 $tareas = explode(";", $tareas);
+// Creo 2 variables auxiliares
 $c = 0;
 $j = 0;
-
+// Este if sirve para si viene el id vacío(tareas[0])
 if($tareas[0] == false){
     $maximo = $contador = 0;
+    // En este while compruebo cual es el id más alto.
     while($b = fgets($a)){
         $tarea = explode(";", $b);
         if($contador == 0){
@@ -17,25 +21,25 @@ if($tareas[0] == false){
         }
         $contador++;
     }
+    // Aquí compruebo que si estoy en la primera vuelta del while y tanto máximo como $contador son igual a 0 significa que el archivo está vacío
     if(($maximo == 0) && ($contador == 0)){
         $nuevatarea = $maximo . ";" . $tareas[1];
         $c = 1;
-        // fwrite ($a, PHP_EOL);
+        // Escribo en el fichero la tarea con el id 0
         fwrite($a, $nuevatarea . "\n");
         fclose($a);
     }else{
         $nuevatarea = $maximo+1 . ";" . $tareas[1];
-        // fwrite ($a, PHP_EOL);
+        // Escribo en el fichero la tarea con el id maximo que hemos obtenido del id más alto +1
         fwrite($a, $nuevatarea . "\n");
         fclose($a);
     }
-  
+//En caso de que el usuario haya ingresado un id entramos en este else
 }else{
     
     $contador = $maximo = 0;
-    
+    // En este while compruebo cual es el id más alto.
     while($b = fgets($a)){  
-
         $tarea = explode(";", $b);
         if($contador == 0){
             $maximo = $tarea[0];
@@ -45,15 +49,18 @@ if($tareas[0] == false){
         }
         $contador++;       
     }
+    // Aquí compruebo que el id que ha ingresado el usuario (tareas[0]) es igual al id de alguna tarea que hayamos encontrado en el fichero
     if($tarea[0] == $tareas[0] && $j == 0){
         echo "Cuidado que este id ya existe.";
         $j = 1;
     }
+    // Aquí compruebo que el id que ha ingresado el usuario no sea superior a un número en el que pueda haber un hueco entre el id máximo del fichero y el que ha ingresado en usuario, por ejemplo usuario ingresa 13
+    // y en el fichero existe 11, como hay un hyeco que es el 12 pues saltaría este if
     if($tareas[0] > $maximo+1 && $c == 0 && $j == 0){
         echo "Hay un hueco entre las ids";
         $c = 1;
     }
-
+// En el caso de que no haya slatado ninguno de los 2 anteriores ifs escribo dentro del fichero la nueva tarea que ha ingresado el usuario
     if($j == 0 && $c == 0){
         $nuevatarea = $tareas[0] . ";" . $tareas[1];
         fwrite($a, $nuevatarea . "\n");
