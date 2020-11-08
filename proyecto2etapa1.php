6df7e1f72d7869049f43434741ad81a3ababf7bb
AlbertoBallesteros <73884993+AlbertoBallesteros@users.noreply.github.com>
2020-11-08 23:32:57 +0100

    Update proyecto2etapa1.php

--- proyecto2etapa1.php
+++ proyecto2etapa1.php
@@ -16,7 +16,7 @@
                 <?php /*Abro el archivo de texto con el fopen, con el while repito, mientras queden tareas, el mostrar una tarea con el fgets y el echo y hacer un salto de línea con el br , lo cierro con el fclose */
                 $fichero = fopen("pendientes.txt","rb");
                 $contador = 0;
-                /*Recorro todas las líneas del fichero con el while, en la variable $tareas creo una lista dividiendo cada línea por ; el primer elemento que sería [0] es para el id
+                /*Recorro todas las líneas de fichero con el while, en la variable $tareas creo una lista dividiendo cada línea por ; el primer elemento que sería [0] es para el id
                 y el segundo el elemento que sería [1] es para la tarea*/
                 while($a = fgets($fichero)){
                 $tareas = explode(";", $a);
@@ -37,48 +37,22 @@
             </td>
             <td>
                 <?php /*Abro el archivo de texto con el fopen, con el while repito, mientras queden tareas, el mostrar una tarea con el fgets y el echo y hacer un salto de línea con el br , lo cierro con el fclose */
-                $fichero2 = fopen("enprogreso.txt","rb");
-                $contador = 0;
-                /*Recorro todas las líneas del fichero con el while, en la variable $tareas creo una lista dividiendo cada línea por ; el primer elemento que sería [0] es para el id
-                y el segundo el elemento que sería [1] es para la tarea*/
-                while($a2 = fgets($fichero2)){
-                    $tareas = explode(";", $a2);
-                    $id = $tareas[0];
-                    /*En este if estoy comprobando que el id que leo en el documento sea igual que la variable $contador, cuando ambos son 0 muestro en el html el primer elemtno que sería el 0,
-                    y así consecutivamente par amostrarlos todos ordenados*/
-                        if($id == $contador){
-                            echo $id;
-                            echo $tareas[1];
-                            echo "<br>";
-                            $contador++;   
-                            fseek($fichero2, 0);  
-                        }
-    
-                    }
-                    fclose($fichero2); 
+                $fichero = fopen("enprogreso.txt","rb");
+                while($tarea = fgets($fichero)){
+                echo $tarea;
+                echo "<br>";
+                }
+                fclose($fichero); 
                 ?>
             </td>
             <td>
                 <?php /*Abro el archivo de texto con el fopen, con el while repito, mientras queden tareas, el mostrar una tarea con el fgets y el echo y hacer un salto de línea con el br , lo cierro con el fclose */
-                $fichero3 = fopen("finalizadas.txt","rb");
-                $contador = 0;
-                /*Recorro todas las líneas del fichero con el while, en la variable $tareas creo una lista dividiendo cada línea por ; el primer elemento que sería [0] es para el id
-                y el segundo el elemento que sería [1] es para la tarea*/
-                while($a3 = fgets($fichero3)){
-                    $tareas = explode(";", $a3);
-                    $id = $tareas[0];
-                    /*En este if estoy comprobando que el id que leo en el documento sea igual que la variable $contador, cuando ambos son 0 muestro en el html el primer elemtno que sería el 0,
-                    y así consecutivamente par amostrarlos todos ordenados*/
-                        if($id == $contador){
-                            echo $id;
-                            echo $tareas[1];
-                            echo "<br>";
-                            $contador++;   
-                            fseek($fichero3, 0);  
-                        }
-    
-                    }
-                    fclose($fichero3);
+                $fichero = fopen("finalizadas.txt","rb");
+                while($tarea = fgets($fichero)){
+                echo $tarea;
+                echo "<br>";
+                }
+                fclose($fichero); 
                ?>
             </td>
 
